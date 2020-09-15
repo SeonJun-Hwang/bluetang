04c0813693e52bac96eb8daa080454abe01f3a44
SeonJun-Hwang <hsunj2003@naver.com>
2020-09-15 17:12:50 +0900

    Update css
    css 파일의 용도에 맞게 분리
    불필요하게 큰 내용에 대한 내용 분리

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import '~style/index.css';
+import '~style/menu.css';
+import '~style/reactive.css';
+import '~style/fullscreen.css';
 import '~style/animation.css';
 import '~style/notice.css';
 import '~style/modal.css';
-import Controller from './controllers'
+import Controller from './controllers';
 
-window.addEventListener( 'load', () => {
+window.addEventListener('load', () => {
   const controller = new Controller();
   controller.init();
-} );
+});
